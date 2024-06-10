bdefca65093c5bcc9f3a5cedb4f4a235dfb49687
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2024-06-10 22:59:13 +0200

    configured server

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,14 @@ export default defineConfig({
     tsConfigPaths(),
     react(),
   ],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:8080',
+  },
 })
