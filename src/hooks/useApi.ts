f798765474d8820930abd6f7bd5aa984ae086bee
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2024-06-10 22:58:41 +0200

    removed unused vars

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,14 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { AxiosResponse } from "axios";
 import { useEffect, useState, useCallback } from "react";
 
-const FETCH_TRIES_LIMIT = 3;
-
-// const sleep = async (time: number) => new Promise((res, rej) => setTimeout(res, time));
 
 const useApi = <T extends (...args: any) => Promise<AxiosResponse<any, any>>>(apiCall: T, apiParams: Parameters<T>) => {
-  const [tries, setTries] = useState(0);
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState<Awaited<ReturnType<T>>['data'] | null>(null);
   const [error, setError] = useState<unknown>(null);
